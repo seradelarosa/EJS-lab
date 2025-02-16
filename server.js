b0ee3b701659f4dab81f7e2625f6515653b79691
Sera De La Rosa <sera.delarosa@outlook.com>
2025-02-15 23:43:27 -0500

    Created category route and page.

--- server.js
+++ server.js
@@ -59,6 +59,14 @@ app.get('/menu', (req, res) => {
     res.render('menu.ejs', { menu: RESTAURANT.menu });
 });
 
+app.get('/menu/:category', (req, res) => {
+  const category = req.params.category;
+  const menuItems = RESTAURANT.menu.filter(item => item.category === category);
+
+
+  res.render('category.ejs', { category, menuItems,  });
+});
+
 app.listen(3000, () => {
     console.log('listening at port 3000');
 });
